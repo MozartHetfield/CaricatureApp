5be622d3f77c7afc2f10334d94799c4279c76a28
Alexandru Soare <soalexandrion1997@yahoo.com>
2020-01-07 01:20:56 +0200

    caricature - eyes
    caricature - eyes + xml models

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import filters
 import sys
+import caricature
 
 def main():
+    
     input_path = r"./input_photos/" + str(sys.argv[1])
     output_path = r"./output_photos/output_" + str(sys.argv[1])
     mode = int(sys.argv[2]) #0 for normal, 1 for bw, 2 for sepia
 
-    #TODO make feature extraction and exaggeration
+    caricature.caricature(input_path, output_path)
 
-    filters.create_sketch(input_path, output_path, mode)
+    filters.create_sketch(output_path, output_path, mode)
     if (mode == 1):
         filters.black_and_white(output_path, output_path)
     elif (mode == 2):
